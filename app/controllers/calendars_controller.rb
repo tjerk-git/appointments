2e943ee9fd970d9e179889a679bbf21f3dffe302
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2022-05-13 14:54:02 +0200

    csrf

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  skip_before_action :verify_authenticity_token
   before_action :authenticate
 
   def create
