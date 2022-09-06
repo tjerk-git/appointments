91060ecaa00957f366a3c493b5be45bc7bb4081c
Raymond van Dongelen <dongelen@me.com>
2022-09-06 13:10:28 +0200

    Unit testje voor register

--- test/controllers/installation_controller_test.rb
+++ test/controllers/installation_controller_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 class InstallationControllerTest < ActionDispatch::IntegrationTest
   test "register" do
 	assert_difference("Owner.count") do 
-		# request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials('b222d98b-23af-4f1b-ab93-47202a3a9b41')
 		get '/register/300', headers: { 'Authorization':  'Token 		b222d98b-23af-4f1b-ab93-47202a3a9b41'}	
 		assert true
 	end
