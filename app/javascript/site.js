3208995447b99c1a2f0cdb72d08c337ee64d0a50
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2022-12-06 23:08:36 +0100

    fixed header scroll bugs, also stuff

--- app/javascript/site.js
+++ app/javascript/site.js
@@ -4,17 +4,17 @@ function scrollFunction() {
     let header = document.getElementById("header");
     let title = document.getElementById("title");
 
-    console.log(document.body.scrollTop);
-
-    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
-        if (header) {
-            header.style.height = "110px";
-            title.style.fontSize = "20pt";
-        }
-    } else {
-        if (header && title) {
-            header.style.height = "200px";
-            title.style.fontSize = "55pt";
+    if (!header.classList.contains('small__header')) {
+        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+            if (header) {
+                header.style.height = "110px";
+                title.style.fontSize = "20pt";
+            }
+        } else {
+            if (header && title) {
+                header.style.height = "45%";
+                title.style.fontSize = "55pt";
+            }
         }
     }
 }
