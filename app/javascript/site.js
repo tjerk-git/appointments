423861bb298f3d5e716f6badcf3c6ddb0a0da11e
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2022-12-08 16:31:13 +0100

    confetti

--- app/javascript/site.js
+++ app/javascript/site.js
@@ -1,39 +1,32 @@
 
 window.onscroll = function () { scrollFunction() };
 
-
-window.addEventListener("load", () => {
-    let header = document.getElementById("header");
-    let container = document.getElementById("container");
-
-    if (header.classList.contains('small__header')) {
-        container.style.marginTop = "100px";
-        header.style.marginTop = "45px";
-    }
-
-});
+function moreConfetti() {
+    confetti();
+}
 
 
 function scrollFunction() {
     let header = document.getElementById("header");
     let title = document.getElementById("title");
     let container = document.getElementById("container");
+    let logo = document.getElementById("logo");
 
-    if (!header.classList.contains('small__header')) {
-        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+    if (header && title) {
+        if (document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
             if (header) {
                 header.style.height = "110px";
                 header.style.marginTop = "40px";
                 title.style.fontSize = "20pt";
+                logo.style.marginTop = "5px";
                 container.style.marginTop = header.style.height;
             }
         } else {
-            if (header && title) {
-                header.style.height = "200px";
-                header.style.marginTop = "90px";
-                title.style.fontSize = "55pt";
-                container.style.marginTop = header.style.height;
-            }
+            header.style.height = "250px";
+            header.style.marginTop = "90px";
+            title.style.fontSize = "40pt";
+            logo.style.marginTop = "25px";
+            container.style.marginTop = header.style.height;
         }
     }
 }
